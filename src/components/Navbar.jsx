f42e45f16a08fd7ba1ae036b8ba9de268c6ab273
Subham Gyawali <subhamgyawali2063@gmail.com>
2025-06-28 10:03:01 +0545

    initial project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/brain.png"
+function Navbar(){
+return (
+    <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+      Bootstrap
+    </a>
+  </div>
+</nav>)
+}
+export  default Navbar;
