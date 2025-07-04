d83469fc4c5f30bc75cefb7fb05753f6d1e6b462
Subham Gyawali <subhamgyawali2063@gmail.com>
2025-07-04 23:23:11 +0545

    Ecommerce navbar and home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,69 @@
-function Home(){
-    return(
-        <div>
-            <p>this is home page</p>
+import "./Home.css";
+import clothes from "../assets/sweater.jpeg";
+import watch from "../assets/watch.jpeg"
+import pant from "../assets/pant.jpeg"
+import laptop from "../assets/laptop.jpeg"
+import fan from "../assets/fan.jpeg"
+import iphone from "../assets/iphone.jpeg"
+function Home() {
+  return (
+    <>
+      <div className="body">
+        <div className="hero-section">
+          <h1>Shop the latest Product</h1>
+
+          <h3>Explore our new collection</h3>
+
+          <div className="button">
+            <button> Shop Now</button>
+          </div>
         </div>
-    );
+      </div>
+      <div className="product-container">
+        <div className="product">
+          <div className="image-container">
+            <img className="image" src={clothes} alt="clothes"></img>
+          </div>
+          <div className="product-name">sweater</div>
+          <div className="product-name">$10.99</div>
+        </div>
+        <div className="product">
+          <div className="image-container">
+            <img className="image" src={watch} alt="watch"></img>
+          </div>
+          <div className="product-name">watch</div>
+          <div className="product-name">$12.99</div>
+        </div>
+        <div className="product">
+          <div className="image-container">
+            <img className="image" src={pant} alt="pant"></img>
+          </div>
+          <div className="product-name">pant</div>
+          <div className="product-name">$10.99</div>
+        </div>
+        <div className="product">
+          <div className="image-container">
+            <img className="image" src={laptop} alt="laptop"></img>
+          </div>
+          <div className="product-name">laptop</div>
+          <div className="product-name">$100.99</div>
+        </div>
+        <div className="product">
+          <div className="image-container">
+            <img className="image" src={fan} alt="fan"></img>
+          </div>
+          <div className="product-name">fan</div>
+          <div className="product-name">$15.99</div>
+        </div>
+        <div className="product">
+          <div className="image-container">
+            <img className="image" src={iphone} alt="iphone"></img>
+          </div>
+          <div className="product-name">i-phone</div>
+          <div className="product-name">$110.99</div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,60 @@
 import "./Navbar.css";
-import logo from "../assets/pic.jpeg";
+import logo from "../assets/minimart.jpeg";
+import { FaCartPlus } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoMdMenu } from "react-icons/io";
+function Navbar() {
+  return (
+    <>
+   
+      <nav className="navbar">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img
+              src={logo}
+              alt="minimart"
+              width="30"
+              height="24"
+              class="d-inline-block align-text-top"/>
+              Minimart 
+            
+          </a>
+          <ul class="abc">
+            <li>
+              <a href="#"> Home</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">products</a>
+            </li>
+            
+            
+            
+          </ul>
+          <div className="apple">
+          <input type="text" placeholder="search" className="box"></input>
+        <FaSearch size={20} />
+        </div>
+        <div class="cart">
 
-function Navbar(){
-return (
-    <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src={logo} alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
-      Bootstrap
-    </a>
-  </div>
-</nav>)
+        <a href="#"><FaShoppingCart size={20}/></a>
+        <a href="#"><FaUserAlt /></a>
+        <a href="#"><IoMdMenu  size={25}/></a>
+        </div>
+        
+
+        </div>
+      </nav>
+      
+    </>
+  );
 }
-export  default Navbar;
+export default Navbar;
